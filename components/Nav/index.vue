22624a8fbb4dceb31665cb84d80cfa89ce07e799
buzzinJohnnyBoi <john_campbell@email.com>
2025-01-25 23:21:20 -0500

    updates and add test image

---
+++ components/Nav/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav class="bg-gray-700 text-white text-lg p-4 border-b-2">
+      <ul class="flex space-x-4">
+        <li>
+          <NuxtLink to="/" class="hover:underline hover:text-green-500">
+            Home
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
+<script lang="ts">
+useHead({
+  title: "John Campbell Software Developer",
+  meta: [
+    {
+      name: "description",
+      content: "John Campbell Software Developer Site",
+    },
+  ],
+});
+</script>
+
 <template>
-  <div class="w-screen h-screen bg-gray-800 text-white">
+  <div class="w-full h-full bg-blue-900 text-white">
     <h1 class="text-3xl font-bold underline">
       John Campbell Professional Site!
     </h1>
+    <img
+      src="/media/john-campbell.png"
+      alt="Description of image"
+      class="mt-4 w-[500px]"
+    />
   </div>
 </template>
