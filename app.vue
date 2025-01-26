bd5a35ed572380a64400d6c5b5a0a7f43447314a
buzzinJohnnyBoi <john_campbell@email.com>
2025-01-25 22:33:47 -0500

    update to test the CI / CD pipeline

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="w-screen h-screen bg-gray-800 text-white">
-    <h1 class="text-3xl font-bold underline">
-      John Campbell Professional Site!
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-screen h-screen bg-gray-800 text-white">
+    <h1 class="text-3xl font-bold underline">
+      John Campbell Professional Site!
+    </h1>
+  </div>
+</template>
