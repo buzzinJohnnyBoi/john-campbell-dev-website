486bbae762326dc8d59c92f663fd54dbbffc8cfa
buzzinJohnnyBoi <john_campbell@email.com>
2025-01-25 22:00:32 -0500

    added tailwind and playwright

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-screen h-screen bg-gray-800 text-white">
+    <h1 class="text-3xl font-bold underline">
+      John Campbell Professional Site!
+    </h1>
   </div>
 </template>
