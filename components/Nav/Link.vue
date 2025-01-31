01c30fe6869d6c8d3c7d150e61c8a5555bb61174
buzzinJohnnyBoi <john_campbell@email.com>
2025-01-30 19:14:42 -0500

    updates to homepage and nav

---
+++ components/Nav/Link.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  link: string;
+  name: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="props.link"
+    class="m-2 p-2 px-3 rounded hover:bg-gray-900 hover:text-green-500"
+  >
+    {{ props.name }}
+  </NuxtLink>
+</template>
--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts"></script>
+
 <template>
   <div>
     <nav class="bg-gray-700 text-white text-lg p-4 border-b-2">
       <ul class="flex space-x-4">
         <li>
-          <NuxtLink to="/" class="hover:underline hover:text-green-500">
-            Home
-          </NuxtLink>
+          <NuxtLink to="/" class="jcampbell-logo"> John Campbell </NuxtLink>
+          <NavLink name="About" link="/about" />
+          <NavLink name="Projects" link="/projects" />
+          <NavLink name="Something" link="/something" />
         </li>
       </ul>
     </nav>
