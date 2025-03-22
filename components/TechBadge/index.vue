daf0f04241cd704f427f3adf12f00322e69a4224
buzzinJohnnyBoi <john_campbell@email.com>
2025-03-22 12:54:12 -0400

    added royce ayr projects to experience page

--- components/TechBadge/index.vue
+++ components/TechBadge/index.vue
@@ -1,42 +1,5 @@
 <script lang="ts" setup>
-const technoligies = {
-  TypeScript: {
-    color: "007ACC",
-    logo: "typescript",
-    logoColor: "white",
-    link: "https://www.typescriptlang.org/",
-  },
-  JavaScript: {
-    color: "F7DF1E",
-    logo: "javascript",
-    logoColor: "black",
-    link: "https://www.javascript.com/",
-  },
-  "Node.js": {
-    color: "43853D",
-    logo: "node.js",
-    logoColor: "white",
-    link: "https://nodejs.org/",
-  },
-  "Express.js": {
-    color: "404d59",
-    logo: "express",
-    logoColor: "61DAFB",
-    link: "https://expressjs.com/",
-  },
-  "Office.js": {
-    color: "2D8CFF",
-    logo: "microsoft-office",
-    logoColor: "white",
-    link: "https://learn.microsoft.com/en-us/office/dev/add-ins/quickstarts/outlook-quickstart-yo",
-  },
-  React: {
-    color: "20232A",
-    logo: "react",
-    logoColor: "61DAFB",
-    link: "https://reactjs.org/",
-  },
-};
+import { technoligies } from "./techTypes";
 
 const props = defineProps<{
   tech: keyof typeof technoligies;
@@ -46,9 +9,14 @@ const selectedTech = technoligies[props.tech];
 </script>
 
 <template>
-  <a :href="selectedTech.link" class="hover:opacity-80 transition-opacity">
+  <a
+    :href="selectedTech.link"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="hover:opacity-80 transition-opacity"
+  >
     <img
-      :src="`https://img.shields.io/badge/${props.tech}-${selectedTech.color}?style=for-the-badge&logo=${selectedTech.logo}&logoColor=white`"
+      :src="`https://img.shields.io/badge/${props.tech}-${selectedTech.color}?style=for-the-badge&logo=${selectedTech.logo}&logoColor=${selectedTech.logoColor}`"
       :alt="props.tech"
     />
   </a>
--- pages/experience.vue
+++ pages/experience.vue
@@ -22,58 +22,117 @@
       </div>
 
       <div class="text-gray-400 mb-4">
-        Responsiblities included developing and deploying various applications,
-        as well as managing IT systems such as physical servers and computers.
+        <p>
+          Responsiblities included developing and deploying various
+          applications, as well as managing IT systems such as physical servers
+          and computers.
+        </p>
+        <p>
+          My boss and I were the only developers at the company, and I was the
+          only one who was a full-stack dev, so projects below I wrote all of
+          the code for, minus a few complex SQL queries.
+        </p>
       </div>
-
       <div class="mt-6">
         <h4 class="font-semibold text-lg text-gray-200 mb-2">Projects:</h4>
 
-        <div class="bg-gray-700 rounded-md p-4 mb-4">
-          <h5 class="font-medium text-gray-100 mb-2">
-            Outlook Addin / PDF Generator
-          </h5>
-          <p class="text-gray-400 mb-1">
-            Developed a Microsoft Outlook add-in that uses a node.js / express
-            server to generate and attach PDF documents of customer documents.
-          </p>
+        <ExperienceProject
+          name="Product Website"
+          :link="{
+            href: 'https://products.royceayr.com/',
+            text: 'products.royceayr.com',
+          }"
+          summary="Developed and deployed a website with dynamic user content for Royce's product line."
+          :descriptionArray="[
+            'Uses a unique data structure with categories, subcategories, series categories, and products, with the series having multiple parent subcategories.',
+            'Customers have the ability to sort by product properties, machine type, machine manufacturer, or industry / application.',
+            'Dealers have the ability to log in to see their pricing and stock quantities. This data is pulled from Royce\'s ERP system.',
+            'Admin Portal is hosted internally, and allows for modifcation of all data on the site. The data can be published to the live site with the click of a button.',
+            'SEO with dynamic sitemap generation.',
+            'CI / CD pipeline setup with GitHub Actions, using Playwright for e2e testing and Jest for unit testing. Deployed on a Ubuntu VPS with OpenTelemetry for monitoring, Prometheus for metrics, Loki for logs, Tempo for tracing, and Grafana for visualization.',
+          ]"
+          :techArray="[
+            'TypeScript',
+            'Node.js',
+            'Next.js',
+            'React',
+            'Bootstrap',
+            'Drizzle',
+            'PostgreSQL',
+            'GitHub Actions',
+            'Playwright',
+            'Jest',
+            'OpenTelemetry',
+            'Prometheus',
+            'Grafana',
+          ]"
+        />
 
-          <ul class="p-2">
-            <li class="text-gray-400 mb-1">
-              Solves a problem with the difficulty of obtaining PDF copies of
-              orders / quotes / invoices for customers.
-            </li>
-            <li class="text-gray-400 mb-1">
-              This former process for obtaining these documents would either be
-              having the ERP email it to the employee who would then forward it
-              to the customer or going into the filing cabinet and scanning it
-              to the customer.
-            </li>
-            <li class="text-gray-400 mb-1">
-              Because of the nature of Royce's business, ~100 documents were
-              being requested per day.
-            </li>
-            <li class="text-gray-400 mb-1">
-              The new system that I developed is an Outlook add-in that allows
-              the user to select the document type (order, quote, etc), enter in
-              the UID for the document, and the application will pull the data
-              off the system, create the PDF and attach it to the email.
-            </li>
-            <li class="text-gray-400 mb-1">
-              Any employee can now access any document in seconds because of
-              this.
-            </li>
-          </ul>
+        <ExperienceProject
+          name="Scanning 007"
+          summary="Developed and deployed reporting / management software for production."
+          :descriptionArray="[
+            'Allows the managers to schedule jobs, set priorites, view job durations, and report pieces completed.',
+            'Allows employees on the shop floor to see priorites and easily log setup / run times for single / group jobs and report pieces (scan)',
+            'Also has a timecard portion for employees to log their hours, and a reporting section for managers to see the data.',
+            'Uses a complicated formula to calculate the projected inventory (as some parts are component parts of other parts)',
+            'The 7th iteration of the application, hence the name.',
+          ]"
+          :techArray="[
+            'TypeScript',
+            'Node.js',
+            'Next.js',
+            'React',
+            'Bootstrap',
+            'Drizzle',
+            'MySQL',
+            'Playwright',
+            'Jest',
+          ]"
+        />
 
-          <div class="flex flex-wrap gap-2">
-            <TechBadge tech="TypeScript" />
-            <TechBadge tech="JavaScript" />
-            <TechBadge tech="Node.js" />
-            <TechBadge tech="Express.js" />
-            <TechBadge tech="Office.js" />
-            <TechBadge tech="React" />
-          </div>
-        </div>
+        <ExperienceProject
+          name="Outlook Addin / PDF Generator"
+          summary="Developed and deployed a Microsoft Outlook add-in that uses a node.js / express server to generate and attach PDF documents of customer documents."
+          :descriptionArray="[
+            'Solves a problem with the difficulty of obtaining PDF copies of orders / quotes / invoices for customers.',
+            'This former process for obtaining these documents would be one of 2 ways, depending on the document: 1) navigate to the correct page in the ERP, and email it to the employee who would then forward it to the customer. Or 2) going into the filing cabinet, scan it to the employee\'s email, and then forward it to the customer.',
+            'Because of the nature of Royce\'s business, ~100 documents were being requested per day.',
+            'The new system that I developed is an Outlook add-in that allows the user to select the document type (order, quote, etc), enter in the UID for the document, and the application will pull the data off the system, create the PDF and attach it to the email.',
+            'Any employee can now access any document in seconds in their outlook client because of this application.',
+          ]"
+          :techArray="[
+            'TypeScript',
+            'JavaScript',
+            'Node.js',
+            'Express.js',
+            'Office.js',
+            'React',
+          ]"
+        />
+
+        <ExperienceProject
+          name="US Shipment Master"
+          summary="Developed and deployed a web app that automates the process of generating US customs documents for shipments to the US."
+          :descriptionArray="[
+            'Replaces a manual process that took ~7 hours a week which now takes ~40 min a week with more accurate data',
+            'Works by taking BOL data from Royce\'s ERP system and finding the appropriate tariffs / country of origins for the items on the BOL',
+            'Generates correct upload files / pdfs for the US customs',
+          ]"
+          :techArray="['JavaScript', 'Node.js', 'Express.js', 'React']"
+        />
+
+        <ExperienceProject
+          name="Drawing Finder App"
+          summary="Developed a desktop app that connects to an API to find drawings."
+          :descriptionArray="[
+            'Royce has over 50,000 drawings on their drawing server orginized by customer, so finding specific drawings can be difficult.',
+            'The solution: a Node.js application connects to the file server and indexes the files once a day. (using cron)',
+            'A rust desktop app connects to the Node.js server and searches the index for the part number.',
+            'If the part number file path is found, the rust desktop app opens the file in the default file viewer.',
+          ]"
+          :techArray="['Rust', 'JavaScript', 'Node.js', 'Express.js']"
+        />
       </div>
     </div>
   </div>
